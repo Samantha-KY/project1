ab629e73e9d7251ca552436f27528e664ee61938
Samantha Murera Kayitete <86477268+Samiskina@users.noreply.github.com>
2022-06-06 14:44:26 +0200

    Initial commit

---
+++ src/main/java/testing.java
@@ -0,0 +1,19 @@
+public class testing {
+
+
+        int result;
+
+        public int add(int number1, int number2){
+            result = number1+number2;
+            return result;
+    }
+    public int sub(int number1, int number2){
+        result = number1-number2;
+        return result;
+    }
+    public int mult(int number1, int number2){
+        result = number1*number2;
+        return result;
+    }
+
+}
---
+++ src/test/java/testingTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testingTest {
+
+    @Test
+    public void add() {
+        testing test = new testing();
+        assertEquals(8,test.add(3,5));
+    }
+
+    @org.junit.Test
+    public void sub() {
+    }
+
+    @org.junit.Test
+    public void mult() {
+    }
+}
